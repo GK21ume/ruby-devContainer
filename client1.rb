60d8b6d91ee25b92c9590032980fe56ec96dbf00
GK21ume <gk21ume@gmail.com>
2026-06-15 03:06:52 +0000

    update http1.0

--- client1.rb
+++ client1.rb
@@ -8,9 +8,10 @@ port = 'http'
 path = '/~toshi/'
 
 sock = TCPSocket.new host , port
-cmd = 'GET ' + path + "\r\n"
+cmd = 'GET ' + path + " HTTP/1.0\r\n"
 pp cmd
 sock.puts cmd
+sock.puts "\r\n"
 
 while line = sock.gets
   puts line
