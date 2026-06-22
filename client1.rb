de42d7bfb66eb549af0373df4c0969da1d012c81
GK21ume <gk21ume@gmail.com>
2026-06-22 03:02:07 +0000

    update http1.1 & change connection close

--- client1.rb
+++ client1.rb
@@ -8,10 +8,16 @@ port = 'http'
 path = '/~toshi/'
 
 sock = TCPSocket.new host , port
-cmd = 'GET ' + path + " HTTP/1.0\r\n"
+rtn = "\r\n"
+cmd = "GET #{path} HTTP/1.1#{rtn}"
+cmd2 = "Host:#{host}#{rtn}"
+cmd3 = "Connection:close #{rtn}"
+cmd_end = rtn
 pp cmd
 sock.puts cmd
-sock.puts "\r\n"
+sock.puts cmd2
+sock.puts cmd3
+sock.puts cmd_end
 
 isbody = false
 
