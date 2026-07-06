3c22a0d9d031eaac74034e75612bfd7e81616298
GK21ume <gk21ume@gmail.com>
2026-07-06 03:33:02 +0000

    implement http hedder

--- server1.rb
+++ server1.rb
@@ -4,12 +4,24 @@
 require 'socket'
 
 def server s
-  cmd , path , ver = s.gets.split
+  cmd , path , ver = s.gets.split " "
+  # HTTP/1.0として正しく返答
+  # 1行目 HTTP/1.0 200 OK
+  # 2 content-Type text/html
+  # 3 空行
+  # 4 コンテンツ
+  # 最後 ソケットクローズ
   if path == "/"
     pp "index"
+    s.print "HTTP/1.0 200 OK\r\n"
+    s.print "content-Type: text/html\r\n"
+    s.print "\r\n"
     s.puts "index"
   else
     pp "other"
+    s.print "HTTP/1.0 200 OK\r\n"
+    s.print "content-Type: text/html\r\n"
+    s.print "\r\n"
     s.puts "other"
   end
   s.close
